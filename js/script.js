b2e34d5b3d5eaf46549fe45a546e291158a46f02
canCulio <mbeylirey@yahoo.com>
2021-04-30 02:02:10 +0200

    loader

--- js/script.js
+++ js/script.js
@@ -21,3 +21,12 @@ $(function() {
     });
     AOS.init();
 });
+
+let mask = document.querySelector('.mask');
+
+window.addEventListener('load', ()=>{
+    mask.classList.add('hide');
+    setTimeout(()=>{
+        mask.remove();
+     },600);
+})
